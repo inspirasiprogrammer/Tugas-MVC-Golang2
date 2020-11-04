8bce9c5405667fc8ebc43207bf5e1e1722afef3e
inspirasiprogrammer <bDa3qviHDaWS6aL>
2020-11-04 14:04:12 +0700

    Update Tugas

--- tugas/go.mod
+++ tugas/go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/mitchellh/mapstructure v1.3.3
 	github.com/pkg/errors v0.9.1
--- tugas/main.go
+++ tugas/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"tugasmvc2/app/controllers"
 	"tugasmvc2/app/middleware"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +13,10 @@ func main() {
 	router.Use(cors.Default())
 	router.POST("/api/v1/account/add", controllers.CreateAccount)
 	router.POST("/api/v1/login", controllers.Login)
-	router.GET("/api/v1/account",middleware.Auth,controllers.GetAccount)
-	router.POST("/api/v1/transfer",middleware.Auth,controllers.Transfer)
-	router.POST("/api/v1/withdraw",middleware.Auth,controllers.Withdraw)
-	router.POST("/api/v1/deposit",middleware.Auth,controllers.Deposit)
-	router.POST("/api/v1/interest",middleware.Auth,controllers.Interest)
-	router.Run(":5000")
+	router.GET("/api/v1/account", middleware.Auth, controllers.GetAccount)
+	router.POST("/api/v1/transfer", middleware.Auth, controllers.Transfer)
+	router.POST("/api/v1/withdraw", middleware.Auth, controllers.Withdraw)
+	router.POST("/api/v1/deposit", middleware.Auth, controllers.Deposit)
+	router.POST("/api/v1/interest", middleware.Auth, controllers.Interest)
+	router.Run(":8080")
 }
