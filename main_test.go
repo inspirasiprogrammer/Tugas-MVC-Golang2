7d6da3a8be8aba83e0e0f98592f0ca98a25cea29
Irwan Syahputra <56063155+inspirasiprogrammer@users.noreply.github.com>
2020-11-10 06:52:26 +0700

    Update Test Account and Mutasi

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -29,7 +30,7 @@ func TestUserSignup(t *testing.T) {
 		{`{"name":""}`, BadRequestCode, "", 0},
 		{`{"name":"Irwan Syahputra","password":"123456"}`, BadRequestCode, "", 0},
 		{`{"name":"Irwan Syahputra","password":"123456","saldo" : 1000000}`, SuccessRequestCode, "", 0},
-		{`{"name":"Irwan Syahputra","password":"123456","saldo" : 1000000}`, BadRequestCode, "", 0}, // request should fail because of
+		{`{"name":"Irwan Syahputra","password":"12456","saldo" : 1000000}`, BadRequestCode, "", 0}, // request should fail because of
 	}
 
 	for i, testCase := range tests {
@@ -63,7 +64,7 @@ func TestUserSignin(t *testing.T) {
 		{`{}`, BadRequestCode, "", 0},
 		{`{"name":""}`, BadRequestCode, "", 0},
 		{`{"name":"Irwan Syahputra","password":"123456"}`, SuccessRequestCode, "", 0},
-		{`{"name":"Irwan Syahputra","password":"123456"}`, BadRequestCode, "", 0}, // request should fail because of
+		{`{"name":"Irwan Syahputra","password":"12456"}`, BadRequestCode, "", 0}, // request should fail because of
 	}
 
 	for i, testCase := range tests {
@@ -88,37 +89,75 @@ func TestUserSignin(t *testing.T) {
 	DisplayTestCaseResults("UserSignin", tests, t)
 }
 
+type List struct{}
+
+func (l List) Handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Request-Id", r.Header.Get("X-Request-Id"))
+
+	w.WriteHeader(http.StatusOK)
+
+	_, _ = w.Write([]byte("inanzzz"))
+}
 func TestUserGetAccountTrans(t *testing.T) {
-
 	url := "http://localhost:8081/api/v1/account/"
+	req := httptest.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("X-Request-Id", "Test-Header")
+	req.Header.Set("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X251bWJlciI6ODMyNzEyLCJuYW1lIjoiSXJ3YW4gU3lhaHB1dHJhIn0.0-Ivg0qYyl3-8iWxhOjQ55bAb9jcrEt25bJBuZCP408")
+	res := httptest.NewRecorder()
 
-	tests := []TestStruct{
-		{`{}`, BadRequestCode, "", 0},
-		{`{"name":""}`, BadRequestCode, "", 0},
-		{`{"name":"Irwan Syahputra","password":"123456"}`, SuccessRequestCode, "", 0},
-		{`{"name":"Irwan Syahputra","password":"123456"}`, BadRequestCode, "", 0}, // request should fail because of
+	list := List{}
+	list.Handle(res, req)
+
+	result := res.Result()
+
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		t.Error(err)
 	}
+	result.Body.Close()
 
-	for i, testCase := range tests {
-
-		var reader io.Reader
-		reader = strings.NewReader(testCase.requestBody) //Convert string to reader
-
-		request, err := http.NewRequest("POST", url, reader)
-
-		res, err := http.DefaultClient.Do(request)
-
-		if err != nil {
-			t.Error(err) //Something is wrong while sending request
-		}
-		body, _ := ioutil.ReadAll(res.Body)
-
-		tests[i].responseBody = strings.TrimSpace(string(body))
-		tests[i].observedStatusCode = res.StatusCode
-
+	if http.StatusOK != result.StatusCode {
+		t.Error("wanted", http.StatusOK, "got", result.StatusCode)
 	}
+	if "Test-Header" != result.Header.Get("X-Request-Id") {
+		t.Error("wanted Test-Header got", result.Header.Get("X-Request-Id"))
+	}
+	if "inanzzz" != string(body) {
+		t.Error("wanted inanzzz got", string(body))
+	}
+}
 
-	DisplayTestCaseResults("UserSignin", tests, t)
+func TestUserGetAccountMutasi(t *testing.T) {
+	url := "http://localhost:8081/api/v1/mutasi/"
+	req := httptest.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("X-Request-Id", "Test-Header")
+	req.Header.Set("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X251bWJlciI6ODMyNzEyLCJuYW1lIjoiSXJ3YW4gU3lhaHB1dHJhIn0.0-Ivg0qYyl3-8iWxhOjQ55bAb9jcrEt25bJBuZCP408")
+
+	res := httptest.NewRecorder()
+
+	list := List{}
+	list.Handle(res, req)
+
+	result := res.Result()
+
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	result.Body.Close()
+
+	if http.StatusOK != result.StatusCode {
+		t.Error("wanted", http.StatusOK, "got", result.StatusCode)
+	} else {
+		t.Logf("responseBody : %s", result.Body)
+	}
+	if "Test-Header" != result.Header.Get("X-Request-Id") {
+		t.Error("wanted Test-Header got", result.Header.Get("X-Request-Id"))
+	}
+	if "inanzzz" != string(body) {
+		t.Error("wanted inanzzz got", string(body))
+	}
 }
 
 func DisplayTestCaseResults(functionalityName string, tests []TestStruct, t *testing.T) {
