56b83d8f14c289cb8261e9169a28dd46bc6acf7c
inspirasiprogrammer <bDa3qviHDaWS6aL>
2020-11-10 13:12:20 +0700

    Update Tugas Debugging and Testing

--- be01gofire/controller/cGuest/c_guest.go
+++ be01gofire/controller/cGuest/c_guest.go
@@ -2,166 +2,164 @@ package cGuest
 
 import (
 	"be01gofire/controller"
-	"be01gofire/model/mAccounts"
-	"be01gofire/model/mQueue"
-	"be01gofire/model/mUser"
+	"be01gofire/model/mBank"
+	"be01gofire/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"time"
 )
 
-func Login(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_login.html`, gin.H{
-			`foo`: `bar`,
-		})
-		return
-	}
-	user := mUser.User{}
-	err := ctx.ShouldBindJSON(&user)
-	pass := user.Pass
-	res := map[string]interface{}{}
-	if err == nil {
-		err := user.FindByEmail(ctx.Firestore)
-		if err == nil {
-			if user.CheckPass(pass) {
-				res[`email`] = user.Email
-				// TODO: set cookie/jwt
-			} else {
-				res[`error`] = `wrong username or password` // wrong password
-			}
-		} else {
-			res[`error`] = `wrong username or password` // not exists
-		}
-	}
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	ctx.JSON(http.StatusOK, res)
-}
+//func Login(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_login.html`, gin.H{
+//			`foo`: `bar`,
+//		})
+//		return
+//	}
+//	user := mUser.User{}
+//	err := ctx.ShouldBindJSON(&user)
+//	pass := user.Pass
+//	res := map[string]interface{}{}
+//	if err == nil {
+//		err := user.FindByEmail(ctx.Firestore)
+//		if err == nil {
+//			if user.CheckPass(pass) {
+//				res[`email`] = user.Email
+//				// TODO: set cookie/jwt
+//			} else {
+//				res[`error`] = `wrong username or password` // wrong password
+//			}
+//		} else {
+//			res[`error`] = `wrong username or password` // not exists
+//		}
+//	}
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	ctx.JSON(http.StatusOK, res)
+//}
+//
+//func Register(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_register.html`, gin.H{
+//			`title`:`Register ` + time.Now().Format(time.RFC1123Z), 
+//		})
+//		return
+//	}
+//	// https://github.com/gin-gonic/gin#bind-form-data-request-with-custom-struct 
+//	user := mUser.User{}
+//	err := ctx.ShouldBindJSON(&user)
+//	res := map[string]interface{}{}
+//	if err == nil {
+//		user.HashPassword()
+//		err = user.Insert(ctx.Firestore)
+//		if err == nil {
+//			res[`user`] = user
+//		}
+//	}
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	ctx.JSON(http.StatusOK, res)
+//}
+//
+//func AddQueue(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_add-queue.html`, gin.H{})
+//		return
+//	}
+//	qe := mQueue.QueueEntry{}
+//	err := ctx.ShouldBindJSON(&qe)
+//	res := map[string]interface{}{}
+//	if err == nil {
+//		err = qe.Insert(ctx.Firestore)
+//		if err == nil {
+//			res[`entry`] = qe
+//		}
+//	}
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	ctx.JSON(http.StatusOK, res)	
+//}
+//func UpdateQueue(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_update-queue.html`, gin.H{})
+//		return
+//	}
+//	qe := mQueue.QueueEntry{}
+//	err := ctx.ShouldBindJSON(&qe)
+//	res := map[string]interface{}{}
+//	if err == nil {
+//		err = qe.Update(ctx.Firestore)
+//		if err == nil {
+//			res[`entry`] = qe
+//		} else {
+//			res[`error`] = `entry not found`
+//		}
+//	}
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	ctx.JSON(http.StatusOK, res)
+//}
+//func RemoveQueue(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_remove-queue.html`, gin.H{})
+//		return
+//	}
+//	qe := mQueue.QueueEntry{}
+//	err := ctx.ShouldBindJSON(&qe)
+//	res := map[string]interface{}{}
+//	if err == nil {
+//		err = qe.Delete(ctx.Firestore)
+//		if err == nil {
+//			res[`entry`] = qe
+//		} else {
+//			res[`error`] = `entry not found`
+//		}
+//	}
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	ctx.JSON(http.StatusOK, res)
+//	
+//}
+//
+//type ShowQueueInput struct {
+//	First int
+//}
+//
+//func ShowQueue(ctx *controller.Ctx) {
+//	if ctx.Context.Request.Method == `GET` {
+//		ctx.HTML(http.StatusOK, `guest_show-queue.html`, gin.H{})
+//		return
+//	}
+//	qe := mQueue.QueueEntry{}
+//	sqi := ShowQueueInput{}
+//	err := ctx.ShouldBindJSON(&sqi)
+//	res := map[string]interface{}{}
+//	rows, err := qe.List(ctx.Firestore,sqi.First)
+//	if err != nil {
+//		res[`error`] = err.Error()
+//	}
+//	res[`list`] = rows
+//	ctx.JSON(http.StatusOK, res)
+//}
 
-func Register(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_register.html`, gin.H{
-			`title`:`Register ` + time.Now().Format(time.RFC1123Z), 
-		})
-		return
-	}
-	// https://github.com/gin-gonic/gin#bind-form-data-request-with-custom-struct 
-	user := mUser.User{}
-	err := ctx.ShouldBindJSON(&user)
-	res := map[string]interface{}{}
-	if err == nil {
-		user.HashPassword()
-		err = user.Insert(ctx.Firestore)
-		if err == nil {
-			res[`user`] = user
-		}
-	}
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	ctx.JSON(http.StatusOK, res)
-}
-
-func AddQueue(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_add-queue.html`, gin.H{})
-		return
-	}
-	qe := mQueue.QueueEntry{}
-	err := ctx.ShouldBindJSON(&qe)
-	res := map[string]interface{}{}
-	if err == nil {
-		err = qe.Insert(ctx.Firestore)
-		if err == nil {
-			res[`entry`] = qe
-		}
-	}
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	ctx.JSON(http.StatusOK, res)	
-}
-func UpdateQueue(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_update-queue.html`, gin.H{})
-		return
-	}
-	qe := mQueue.QueueEntry{}
-	err := ctx.ShouldBindJSON(&qe)
-	res := map[string]interface{}{}
-	if err == nil {
-		err = qe.Update(ctx.Firestore)
-		if err == nil {
-			res[`entry`] = qe
-		} else {
-			res[`error`] = `entry not found`
-		}
-	}
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	ctx.JSON(http.StatusOK, res)
-}
-func RemoveQueue(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_remove-queue.html`, gin.H{})
-		return
-	}
-	qe := mQueue.QueueEntry{}
-	err := ctx.ShouldBindJSON(&qe)
-	res := map[string]interface{}{}
-	if err == nil {
-		err = qe.Delete(ctx.Firestore)
-		if err == nil {
-			res[`entry`] = qe
-		} else {
-			res[`error`] = `entry not found`
-		}
-	}
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	ctx.JSON(http.StatusOK, res)
-	
-}
-
-type ShowQueueInput struct {
-	First int
-}
-
-func ShowQueue(ctx *controller.Ctx) {
-	if ctx.Context.Request.Method == `GET` {
-		ctx.HTML(http.StatusOK, `guest_show-queue.html`, gin.H{})
-		return
-	}
-	qe := mQueue.QueueEntry{}
-	sqi := ShowQueueInput{}
-	err := ctx.ShouldBindJSON(&sqi)
-	res := map[string]interface{}{}
-	rows, err := qe.List(ctx.Firestore,sqi.First)
-	if err != nil {
-		res[`error`] = err.Error()
-	}
-	res[`list`] = rows
-	ctx.JSON(http.StatusOK, res)
-}
-
+//  curl localhost:8084/guest/create-account -H 'content-type:application/json' -d '{"email":"kiswono@gmail.com","id":1}'
+//{"account":{"ID":1,"id_account":"id-414","name":"","email":"kiswono@gmail.com","password":"$2a$10$SWJcgx9kOWlPSJVRk6r9S.40eirOUKlYKr2P8LBKid6.uFFWHNqZi","account_number":832712,"saldo":0}}%   
 func CreateAccount(ctx *controller.Ctx) {
 	if ctx.Context.Request.Method == `GET` {
 		ctx.HTML(http.StatusOK, `guest_create-account.html`, gin.H{})
 		return
 	}
-	a := mAccounts.Account{}
+	a := mBank.Account{}
 	err := ctx.ShouldBindJSON(&a)
 	res := map[string]interface{}{}
 	if err == nil {
-		err = a.IsValid()
-	}
-	if err == nil {
-		tx := ctx.Db.Create(&a)
-		err = tx.Error
+		pass := utils.HashGenerator(a.Password) 
+		a.Password = pass
+		err = a.InsertNewAccount(ctx.Db)
 		if err == nil {
 			res[`account`] = a
 		} 
@@ -170,4 +168,23 @@ func CreateAccount(ctx *controller.Ctx) {
 		res[`error`] = err.Error()
 	}
 	ctx.JSON(http.StatusOK, res)
+}
+
+// curl localhost:8084/guest/login -H 'content-type:application/json' -d '{"email":"kiswono@gmail.com","password":""}'
+// {"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X251bWJlciI6ODMyNzEyLCJlbWFpbCI6Imtpc3dvbm9AZ21haWwuY29tIn0.VrIftkT0vfYxDWmjvuK_aDiIcevzVitiYtr9QqAMclQ"}%
+func Login(ctx *controller.Ctx) {
+	auth := mBank.Auth{}
+	err := ctx.ShouldBindJSON(&auth)
+	res := map[string]interface{}{}
+	if err == nil {
+		token := ``
+		err, token = auth.Login(ctx.Db)
+		if err == nil {
+			res[`token`] = token
+		}
+	}
+	if err != nil {
+		res[`error`] = err.Error()
+	}
+	ctx.JSON(http.StatusOK,res)
 }
--- be01gofire/controller/server.go
+++ be01gofire/controller/server.go
@@ -1,20 +1,22 @@
 package controller
 
 import (
-	"be01gofire/model/mAccounts"
-	"log"
-
+	"be01gofire/model/mBank"
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"math/rand"
+	"time"
 )
 
 const (
 	FirebaseProject = `test1-1c797`
-	FirebaseConfig  = `firebase.json`
-	MysqlDsn        = `root:@/be01?parseTime=True&charset=utf8`
+	FirebaseConfig = `firebase.json`
+	MysqlDsn = `be01:pass123@/be01?parseTime=True&charset=utf8`
 )
 
 type Server struct {
@@ -25,6 +27,7 @@ type Server struct {
 }
 
 func InitServer() *Server {
+	rand.Seed(time.Now().UnixNano())
 	// connect ke firebase
 	//ctx := context.Background()
 	//opt := option.WithCredentialsFile(FirebaseConfig)
@@ -40,14 +43,21 @@ func InitServer() *Server {
 	// connect ke mysql
 	db, err := gorm.Open(mysql.Open(MysqlDsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf(`failed to connect to mysql: ` + err.Error())
+		log.Fatalf(`failed to connect to mysql: `+err.Error())
 	}
-	db.AutoMigrate(&mAccounts.Account{})
+	db.AutoMigrate(&mBank.Account{},&mBank.Auth{},&mBank.Transaction{})
 	// set struct
 	s := &Server{}
 	//s.App = app
 	//s.Firestore = fire
 	s.Router = gin.Default()
+	// add CORS
+	cfg := cors.DefaultConfig()
+	cfg.AllowAllOrigins = true
+	cfg.AllowCredentials = true
+	cfg.AllowMethods = []string{"GET","POST"}
+	cfg.AllowHeaders = []string{"Authorization","Origin","Accept","X-Requested-With"," Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers"}
+	s.Router.Use(cors.New(cfg))
 	s.Db = db
 	// https://chenyitian.gitbooks.io/gin-web-framework/content/docs/26.html
 	s.Router.LoadHTMLGlob("view/*")
@@ -55,13 +65,19 @@ func InitServer() *Server {
 }
 
 func (s *Server) Listen(port string) {
-	s.Router.Run(port)
+	s.Router.Run(port)	
 }
 
 func (s *Server) AssignHandler(route string, handler Handler) {
-	s.Router.Any(route, func(context *gin.Context) {
+	s.Router.GET(route, func(context *gin.Context) {
 		handler(&Ctx{
-			Server:  s,
+			Server: s,
+			Context: context,
+		})
+	})
+	s.Router.POST(route, func(context *gin.Context) {
+		handler(&Ctx{
+			Server: s,
 			Context: context,
 		})
 	})
